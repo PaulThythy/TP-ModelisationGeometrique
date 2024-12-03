b8ec26c791e37cc01d651f95aac8ff15bbf2d5c6
PaulThythy <paul.thi@hotmail.fr>
2024-12-03 15:26:02 +0100

    TP4 added

---
+++ TP4-automate/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+make clean;
+make;
+#./bin/postscript automateBezier.txt 5
+#./bin/postscript automateCantor.txt 5
+#./bin/postscript automateCantorCarre.txt 5
+./bin/postscript automateCarreSierp.txt 5
