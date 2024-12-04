fed34c44273a1579970dd5d469e925339f7ec1f2
PaulThythy <paul.thi@hotmail.fr>
2024-12-04 17:26:46 +0100

    finished TP4

--- TP4-automate/run.sh
+++ TP4-automate/run.sh
@@ -5,4 +5,4 @@ make;
 #./bin/postscript automateBezier.txt 5
 #./bin/postscript automateCantor.txt 5
 #./bin/postscript automateCantorCarre.txt 5
-./bin/postscript automateCarreSierp.txt 5
+./bin/postscript automateAutreCantorCarreEtCarre.txt 10
